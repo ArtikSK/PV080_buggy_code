e8b6339f060d5946e3a7e1aec68e020bca41a5fe
Patrik Kotucek <524952@muni.cz>
2023-03-29 08:44:19 +0200

    Another fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,12 +1,14 @@
 import sys
 import os
-import yaml
+import urllib
+
 import flask
+import yaml
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
@@ -47,11 +49,12 @@ def authenticate(password):
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
-    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print("1. Format string vulnerability: " +
+          "use string={person.__init__.__globals__[CONFIG][API_KEY]}")
     print("2. Code injection vulnerability: use string=;print('Own code executed') #")
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
-    choice  = input("Select vulnerability: ")
+    choice = input("Select vulnerability: ")
     if choice == "1":
         new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
